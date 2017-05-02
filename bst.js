95f9f501014fa3a628c83af4213f40ca60db0329
Rajesh <rajesh.kathiriya507@gmail.com>
2017-05-02 15:56:06 +0530

    binary search tree

---
+++ bst.js
@@ -0,0 +1,56 @@
+function Node(val){
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+
+function BinarySearchTree(){
+  this.root = null;
+}
+
+
+BinarySearchTree.prototype.push = function(val){
+   var root = this.root;
+
+   if(!root){
+      this.root = new Node(val);
+      return;
+   }
+
+   var currentNode = root;
+   var newNode = new Node(val); 
+
+   while(currentNode){
+      if(val < currentNode.value){
+          if(!currentNode.left){
+             currentNode.left = newNode;
+             break;
+          }
+          else{
+             currentNode = currentNode.left;
+        }
+     }
+     else{
+         if(!currentNode.right){
+            currentNode.right = newNode;
+            break;
+         }
+         else{
+            currentNode = currentNode.right;
+         }
+     }
+  }
+
+}      
+
+function main (){
+    var bst = new BinarySearchTree();
+    bst.push(3);
+    bst.push(2);
+    bst.push(4);
+    bst.push(1);
+    bst.push(5);
+}
+
+main();
---
+++ largest-region-in-boolean-matrix.js
@@ -0,0 +1,20 @@
+function largest(array){
+    var visited = [ [0,0,0,0,0],
+                    [0,0,0,0,0],
+                    [0,0,0,0,0],
+                    [0,0,0,0,0]
+                ]
+}
+
+
+function main(){
+    var array =[ [0,0,1,1,0],
+                 [1,0,1,1,0],
+                 [0,1,0,0,0],
+                 [0,0,0,0,1]
+                ];
+    console.log(array);
+}
+
+main();
+
