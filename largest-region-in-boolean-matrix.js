ae1ab7007fc069f8c3121f2d4f626ed5759d6527
Rajesh <rajesh.kathiriya507@gmail.com>
2017-05-04 19:18:19 +0530

    largest region in boolean matrix

--- largest-region-in-boolean-matrix.js
+++ largest-region-in-boolean-matrix.js
@@ -1,20 +1,63 @@
-function largest(array){
-    var visited = [ [0,0,0,0,0],
-                    [0,0,0,0,0],
-                    [0,0,0,0,0],
-                    [0,0,0,0,0]
-                ]
+function largest(array) {
+    var visited = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+
+    var result = 0;
+    for (var i = 0; i < visited.length; i++) {
+        for (var j = 0; j < visited[0].length; j++) {
+            if (array[i][j] == 1 && visited[i][j] != 1) {
+                count = {
+                    value: 1 
+                };
+                DFS(array, i, j, visited, count); // pass value by reference we have to pass value inside object like we have pointer in C
+
+                result = Math.max(result, count.value);
+            }
+        }
+    }
+
+    return result;
 }
 
 
-function main(){
-    var array =[ [0,0,1,1,0],
-                 [1,0,1,1,0],
-                 [0,1,0,0,0],
-                 [0,0,0,0,1]
-                ];
-    console.log(array);
+function DFS(array, i, j, visited, count) {
+
+    var rowNbr = [-1, -1, -1, 0, 0, 1, 1, 1];
+    var colNbr = [-1, 0, 1, -1, 1, -1, 0, 1];
+
+
+    visited[i][j] = true;
+
+
+    for (var k = 0; k < 8; ++k) {
+        if (isSafe(array, i + rowNbr[k], j + colNbr[k],
+                visited)) {
+            count.value = count.value + 1;
+            DFS(array, i + rowNbr[k], j + colNbr[k],
+                visited, count);
+        }
+    }
+}
+
+function isSafe(array, i, j, visited) {
+    return (i >= 0) && (i < 4) &&
+        (j >= 0) && (j < 5) &&
+        (array[i][j] && !visited[i][j]);
+}
+
+function main() {
+    var array = [
+        [0, 0, 1, 1, 0],
+        [1, 0, 1, 1, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1]
+    ];
+    var result = largest(array);
+    console.log(result);
 }
 
 main();
-
